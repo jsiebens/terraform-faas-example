ff43ec8e25b2ab864ba116bb07cf2b3c492c4149
Johan Siebens <johan.siebens@gmail.com>
2021-12-08 12:50:30 +0100

    test

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 set -x
 
-# Usage: http POST :8080/echo msg==hello foo=bar
+if [ -x "/usr/local/bin/terraform" ]; then
+    echo "Terraform already installed"
+else
+    cd /tmp
+    wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
+    unzip terraform_1.0.11_linux_amd64.zip
+    mv terraform /usr/local/bin/
+    rm -rf terraform_1.0.11_linux_amd64.zip
+fi
 
-echo "This is a simple echo hook."
-echo "Hello world"
-
-cd /tmp
-
-wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
-unzip terraform_1.0.11_linux_amd64.zip
-mv terraform /usr/local/bin/
-
-rm -rf terraform_1.0.11_linux_amd64.zip
 
 /usr/local/bin/terraform version
 
 cd /var/opt/terraform/src
 terraform init
+
+export TF_VAR_openfaas_password=$(cat /run/secrets/basic-auth-password)
+
+terraform plan
