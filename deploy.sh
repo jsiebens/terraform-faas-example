e8fd58049c2649423baf92eb51ba04a67182f351
Johan Siebens <johan.siebens@gmail.com>
2021-12-08 12:38:01 +0100

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -ex
+set -x
 
 # Usage: http POST :8080/echo msg==hello foo=bar
 
@@ -11,3 +11,5 @@ cd /tmp
 wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
 unzip terraform_1.0.11_linux_amd64.zip
 sudo mv terraform /usr/local/bin/
+
+rm -rf terraform_1.0.11_linux_amd64.zip
