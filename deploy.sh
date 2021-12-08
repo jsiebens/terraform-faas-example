3da49eefdaa88c966cde4cc939422480cf560fd2
Johan Siebens <johan.siebens@gmail.com>
2021-12-08 12:36:17 +0100

    bump

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+set -ex
 
 # Usage: http POST :8080/echo msg==hello foo=bar
 
 echo "This is a simple echo hook."
 echo "Hello world"
+
+cd /tmp
+
+wget https://releases.hashicorp.com/terraform/1.0.11/terraform_1.0.11_linux_amd64.zip
+unzip terraform_1.0.11_linux_amd64.zip
+mv terraform /usr/local/bin/
